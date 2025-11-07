3bd3aefa52a4d3d5f7bc3e76a815475cd6693d02
khoi2711 <huynhanhkhoi9a7@gmail.com>
2025-11-07 15:45:12 +0700

    updated log collector

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-from modules.collector.logs_window_collector import collect_windows_logs
-from utils.File_queue import File_queue
+from modules.collector.logs_window_collector import log_collector
 if __name__ == "__main__":
-    logs = collect_windows_logs()
-    queue = File_queue("./data/logs/window_log.queue")
-    queue.push(logs)
-    print(f"Số lượng item trong hàng đợi: {queue.size()}") 
+    logs = log_collector(None,"System Application")
+
+print(f"Đã thu thập thành công")
